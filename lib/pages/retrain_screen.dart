0c614b7b53e1a574ee5efc4b8f7b391aa2f8ef84
IvanAvila1224 <magista1224@gmail.com>
2023-11-08 09:38:32 -0600

    ahora si ya

--- lib/pages/retrain_screen.dart
+++ lib/pages/retrain_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:namer_app/services/reservice.dart';
 
-class RetrainScreen extends StatelessWidget {
-  final FitService fitService = FitService();
+ class RetrainScreen extends StatelessWidget {
+  final FitService fitService = FitService(); // Crear una instancia de FitService
 
+  final List<String> datasetUrls = [
+    "https://firebasestorage.googleapis.com/v0/b/size-clothes.appspot.com/o/final_test_50.csv?alt=media&token=f452ccd6-0e94-4bef-8173-e27cb2d82f8c",
+    "https://firebasestorage.googleapis.com/v0/b/size-clothes.appspot.com/o/final_test.csv?alt=media&token=8303e9c8-31e2-4200-beef-34800d0ed187",
+    "https://firebasestorage.googleapis.com/v0/b/size-clothes.appspot.com/o/final_test_15.csv?alt=media&token=8ffab7c6-e04e-428a-b799-a553d1226468",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,20 +19,26 @@ class RetrainScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                _retrainModel();
-              },
-              child: const Text('Reentrenar Modelo'),
-            ),
+            for (int i = 0; i < datasetUrls.length; i++)
+              Column(
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      _retrainModel(datasetUrls[i]);
+                    },
+                    child: Text('Reentrenar Modelo ${i + 1}'),
+                  ),
+                  SizedBox(height: 16.0), // Espacio vertical entre los botones
+                ],
+              ),
           ],
         ),
       ),
     );
   }
 
-  Future<void> _retrainModel() async {
-    await fitService.retrainModel();
+  Future<void> _retrainModel(String datasetUrl) async {
+    await fitService.retrainModel(datasetUrl);
     // Puedes agregar lógica adicional aquí, como mostrar un mensaje de éxito.
   }
-}
+ }
--- lib/services/reservice.dart
+++ lib/services/reservice.dart
@@ -5,18 +5,15 @@ import 'package:http/http.dart' as http;
 class FitService with ChangeNotifier {
   final String baseUrl = 'https://api.github.com/repos/IvanAvila1224/clothes-size/dispatches';
 
-  Future<void> retrainModel() async {
+  Future<void> retrainModel(String datasetUrl) async {
     final Uri url = Uri.parse(baseUrl);
     print(url);
-
-    final dataseturl =
-        "https://firebasestorage.googleapis.com/v0/b/size-clothes.appspot.com/o/final_test_50.csv?alt=media&token=f452ccd6-0e94-4bef-8173-e27cb2d82f8c";
 
     final requestBody = {
       "event_type": "ml_ci_cd",
       "client_payload": {
-        "dataseturl": dataseturl,
-        "sha": "iva" // Puedes usar un valor único aquí
+        "dataseturl": datasetUrl,
+        "sha": "iv"
       }
     };
     print(jsonEncode(requestBody));
@@ -24,7 +21,7 @@ class FitService with ChangeNotifier {
     final response = await http.post(
       url,
       headers: {
-        'Authorization': 'Bearer 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', // Asegúrate de que el token sea correcto
+        'Authorization': 'Bearer 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', // Asegúrate de que el token sea correcto
         'Accept': 'application/vnd.github.v3+json',
         'Content-type': 'application/json',
       },
